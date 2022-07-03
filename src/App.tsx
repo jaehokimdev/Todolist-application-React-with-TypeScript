5d6b11fda840f358a25560b57c7c5ae7a80183ab
jaehokimdev <101899896+jaehokimdev@users.noreply.github.com>
2022-07-03 17:33:56 -0600

    doing-typescript-part

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import React, { FC } from "react";
+import React, { FC, useState, ChangeEvent } from "react";
 import "./App.css";
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todo, setTodo] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder="Task..." />
-          <input type="number" placeholder="DeadLine (in Days)..." />
+          <input
+            type="text"
+            placeholder="Task..."
+            name="task"
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="DeadLine (in Days)..."
+            name="deadline"
+          />
         </div>
         <button>Add Task</button>
       </div>
